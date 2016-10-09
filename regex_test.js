7acca36dd58e89608891c118853389fa349a6b53
kelvinkfli <kelvinkfli@gmail.com>
2016-10-08 23:53:28 -0400

    Update regex_test.js

--- regex_test.js
+++ regex_test.js
@@ -10,25 +10,25 @@ regexTest = function(match_array, skip_array, input) {
     var match = 0; //this variable tracks the number of match errors
     var skip = 0; //this variable tracks the number of skip errors
     for (var x = 0, y = match_array.length; x < y; x++) {
-        if (input.test(match_array[x]) == false) {
+        if (input.test(match_array[x]) == false) { //if any of the strings_to_match are false, increase match error count
             match += 1;
         }
     }
     for (var x = 0, y = skip_array.length; x < y; x++) {
-        if (input.test(skip_array[x]) == true) {
+        if (input.test(skip_array[x]) == true) { //if any of the strings_to_skip are true, increase skip error count
             skip += 1;
         }
     }
-    if (match > 0 || skip > 0) {
+    if (match > 0 || skip > 0) { //if any errors occurred, regex isn't correct
         console.log('number of match errors: ' + match);
         console.log('number of skip errors: ' + skip);
         console.log('Regex does not meet requirements');
-    } else {
+    } else { //no errors means regex is correct
         console.log('Regex meets all requirements');
     }
 }
 
-regexTest(strings_to_match, strings_to_skip, user_regex_input);
+regexTest(strings_to_match, strings_to_skip, user_regex_input); //test out the function
 
 
 --------------------------------------------------Scoring Logic-------------------------------------------------
